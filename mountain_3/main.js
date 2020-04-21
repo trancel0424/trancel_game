f16bcacc6ced108306705d57822222acb3752dd7
trancel0424 <62266827+trancel0424@users.noreply.github.com>
2020-04-21 22:18:59 +0900

    Add files via upload

---
+++ mountain_3/main.js
@@ -0,0 +1,194 @@
+left_num=0;
+middle_num=0;
+right_num=0;
+
+window.onload=function(){
+  left_num=Math.floor(Math.random()*6+2);
+  middle_num=Math.floor(Math.random()*6+2);
+  right_num=Math.floor(Math.random()*6+2);
+  num_kousin();
+}
+
+function trancel_speak(comment){
+  const p=document.getElementById("trancel");
+  p.textContent=comment;
+}
+function txt_kousin(comment){
+  const p=document.getElementById("txt");
+  p.textContent=comment;
+}
+function num_kousin(){
+  left_kousin();
+  middle_kousin();
+  right_kousin();
+}
+function left_kousin(){
+  const p=document.getElementById("left");
+  p.textContent=left_num;
+}
+function middle_kousin(){
+  const p=document.getElementById("middle");
+  p.textContent=middle_num;
+}
+function right_kousin(){
+  const p=document.getElementById("right");
+  p.textContent=right_num;
+}
+
+function btn1kousin(txt,func){
+  const div=document.getElementById("btn1");
+  div.setAttribute("value",txt);
+  div.setAttribute("onclick",func);
+}
+function btn2kousin(txt,func){
+  const div=document.getElementById("btn2");
+  div.setAttribute("value",txt);
+  div.setAttribute("onclick",func);
+}
+function btn3kousin(txt,func){
+  const div=document.getElementById("btn3");
+  div.setAttribute("value",txt);
+  div.setAttribute("onclick",func);
+}
+
+function senkou(){
+  trancel_speak("お前の番じゃけ！");
+  const div = document.getElementById("btn-wrapper");
+  const input1 = document.createElement("input");
+  input1.setAttribute("type","button");
+  input1.setAttribute("class","btn");
+  input1.setAttribute("id","btn3");
+  div.appendChild(input1);
+  player_turn();
+}
+
+function koukou(){
+  const div = document.getElementById("btn-wrapper");
+  const input1 = document.createElement("input");
+  input1.setAttribute("type","button");
+  input1.setAttribute("class","btn");
+  input1.setAttribute("id","btn3");
+  input1.setAttribute("value","(눈_눈)");
+  div.appendChild(input1);
+  trancel_turn();
+}
+
+function player_turn(){
+  txt_kousin("どの山からとりますか？")
+  btn1kousin("左の山","left_dec()");
+  btn2kousin("真ん中の山","middle_dec()");
+  btn3kousin("右の山","right_dec()");
+}
+
+function left_dec(){
+  if(left_num==0){
+    txt_kousin("その山はもう減らせません！");
+  }
+  else{
+    txt_kousin("もっと減らしたければ「減らす」を押してください。");
+    left_num-=1;
+    left_kousin();
+    btn1kousin("減らす","left_dec_number()");
+    btn2kousin("終了","trancel_turn();");
+    btn3kousin("(눈_눈)","")
+  }
+}
+function left_dec_number(){
+  if(left_num>0){
+    left_num-=1;
+  }
+  left_kousin();
+}
+
+function middle_dec(){
+  if(middle_num==0){
+    txt_kousin("その山はもう減らせません！");
+  }
+  else{
+    txt_kousin("もっと減らしたければ「減らす」を押してください。");
+    middle_num-=1;
+    middle_kousin();
+    btn1kousin("減らす","middle_dec_number()");
+    btn2kousin("終了","trancel_turn();");
+    btn3kousin("(눈_눈)","")
+  }
+}
+function middle_dec_number(){
+  if(middle_num>0) middle_num-=1;
+  middle_kousin();
+}
+
+function right_dec(){
+  if(right_num==0){
+    txt_kousin("その山はもう減らせません！");
+  }
+  else{
+    txt_kousin("もっと減らしたければ「減らす」を押してください。");
+    right_num-=1;
+    right_kousin();
+    btn1kousin("減らす","right_dec_number()");
+    btn2kousin("終了","trancel_turn();");
+    btn3kousin("(눈_눈)","");
+  }
+}
+function right_dec_number(){
+  if(right_num>0) right_num-=1;
+  right_kousin();
+}
+
+function trancel_turn(){
+  if(right_num==0 && middle_num==0 && left_num==0){
+    trancel_speak("俺の負けじゃけ......");
+    txt_kousin("あなたの勝ちです！！おめでとう！！");
+    btn1kousin("(눈_눈)......","");
+    btn2kousin("(눈_눈)......","");
+    btn3kousin("(눈_눈)......","");
+  }
+  else{
+    trancel_algo(left_num,middle_num,right_num);
+    if(right_num==0 && middle_num==0 && left_num==0){
+      trancel_speak("俺の勝ち！！！！");
+      txt_kousin("あなたの負けです");
+      btn1kousin("(눈_눈)やーい","");
+      btn2kousin("(눈_눈)やーい","");
+      btn3kousin("(눈_눈)やーい","");
+    }
+    else{
+      trancel_speak("はい！お前の番じゃけ！");
+      player_turn();
+    }
+  }
+}
+
+function trancel_algo(a,b,c){
+  a=a.toString(2);
+  b=b.toString(2);
+  c=c.toString(2);
+
+  var x = a^b^c;
+
+  if(x==0){
+    max=a;
+    if(max<b){max=b;}
+    if(max<c){max=c;}
+    if(max==a){left_num-=1;}
+    if(max==b){middle_num-=1;}
+    if(max==c){right_num-=1;}
+  }
+  else{
+    j = 0;
+    if (a>(a^x)){
+      a=a^x;
+      left_num=parseInt(a,2);
+    }
+    else if(b>(b^x)){
+      b=b^x;
+      middle_num=parseInt(b,2);
+    }
+    else{
+      c=c^x;
+      right_num=parseInt(c,2);
+    }
+  }
+  num_kousin();
+}
---
+++ number/main.js
@@ -0,0 +1,142 @@
+var now_num=0;
+var out_num=0; //言ったら負けの数字、読み込み時に更新
+//var num=0; //１ターンで言える数、読み込み時更新
+
+window.onload=function(){
+  out_num=Math.floor(Math.random()*20 + 40);
+  //num=Math.floor(Math.random()*3 + 2);
+  const p=document.getElementById("txt_up");
+  p.textContent=`アウト数は${out_num}です。`;
+  now_num_print(now_num);
+}
+function now_num_print(n){
+  const p=document.getElementById("txt_now");
+  p.textContent=`現在の数字は${n}です。`;
+}
+function trancel(comment){
+  const p=document.getElementById("trancel");
+  p.textContent=comment;
+}
+
+function senkou(){
+  const div2 = document.getElementById("btn-wrapper");
+  const input1 = document.createElement("input");
+	input1.setAttribute("type","button");
+	input1.setAttribute("class","btn");
+  input1.setAttribute("id","btn3");
+  div2.appendChild(input1);
+  btn1kousin(now_num);
+  btn2kousin(now_num);
+  btn3kousin(now_num);
+  trancel("お前が先行じゃけ!始めるじゃけ!");
+}
+function koukou(){
+  const div2 = document.getElementById("btn-wrapper");
+  const input1 = document.createElement("input");
+	input1.setAttribute("type","button");
+	input1.setAttribute("class","btn");
+  input1.setAttribute("id","btn3");
+  div2.appendChild(input1);
+  btn1kousin(now_num);
+  btn2kousin(now_num);
+  btn3kousin(now_num);
+  trancel_turn(now_num);
+}
+function btn1kousin(n){
+  const div=document.getElementById("btn1");
+  div.setAttribute("value",`${n+1}`);
+  div.setAttribute("onclick","syori1();");
+}
+function btn2kousin(n){
+  const div=document.getElementById("btn2");
+  div.setAttribute("value",`${n+1},${n+2}`);
+  div.setAttribute("onclick","syori2();");
+}
+function btn3kousin(n){
+  const div=document.getElementById("btn3");
+  div.setAttribute("value",`${n+1},${n+2},${n+3}`);
+  div.setAttribute("onclick","syori3();");
+}
+
+function trancel_turn(n){
+  if(n>=out_num){
+    trancel("俺の勝ち！なんで負けたか明日まで考えとくじゃけ！！");
+    youlose1();
+    youlose2();
+    youlose3();
+  }
+  else if(n==out_num-1){
+    trancel(`${out_num}、俺の負けじゃけ......`);
+    youwin1();
+    youwin2();
+    youwin3();
+  }
+  else{
+    var sep=(out_num-now_num-1)%4;
+    if(sep==0) sep=Math.floor(Math.random()*3+1); //勝てないときはやけくそ
+    if(sep==1){
+      trancel(`俺のターンじゃけ、${n+1}！`);
+      now_num+=1;
+    }
+    else if (sep==2) {
+      trancel(`俺のターンじゃけ、${n+1},${n+2}！`);
+      now_num+=2;
+    }
+    else {
+      trancel(`俺のターンじゃけ、${n+1},${n+2},${n+3}！`);
+      now_num+=3;
+    }
+    now_num_print(now_num);
+    btn1kousin(now_num);
+    btn2kousin(now_num);
+    btn3kousin(now_num);
+  }
+}
+
+function syori1(){
+  now_num+=1;
+  now_num_print(now_num);
+  trancel_turn(now_num);
+}
+function syori2(){
+  now_num+=2;
+  now_num_print(now_num);
+  trancel_turn(now_num);
+}
+function syori3(){
+  now_num+=3;
+  now_num_print(now_num);
+  trancel_turn(now_num);
+}
+
+function youwin1(){
+  const div=document.getElementById("btn1");
+  div.setAttribute("value","うれしい！");
+  div.setAttribute("onclick","");
+}
+function youwin2(){
+  const div=document.getElementById("btn2");
+  div.setAttribute("value","うれしい！");
+  div.setAttribute("onclick","");
+}
+function youwin3(){
+  const div=document.getElementById("btn3");
+  div.setAttribute("value","うれしい！");
+  div.setAttribute("onclick","");
+}
+
+function youlose1(){
+  const div=document.getElementById("btn1");
+  div.setAttribute("value","くやしい");
+  div.setAttribute("onclick","");
+}
+function youlose2(){
+  const div=document.getElementById("btn2");
+  div.setAttribute("value","くやしい");
+  div.setAttribute("onclick","");
+}
+function youlose3(){
+  const div=document.getElementById("btn3");
+  div.setAttribute("value","くやしい");
+  div.setAttribute("onclick","");
+}
